438d81e09250f6b7166ea2e6b4b70b39dd7c0eda
Jiaxiang233 <1272718774@qq.com>
2021-12-03 00:38:51 -0500

    场景间转场动画1.1和1.2

--- 遗忘公社/Assets/Scripts/SceneChanger.cs
+++ 遗忘公社/Assets/Scripts/SceneChanger.cs
@@ -6,11 +6,29 @@ using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
     public int SceneIndex;
+
+    public Animator transition;
+
+    public float transTime = 1f;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
         {
-            SceneManager.LoadScene(SceneIndex);
+            LoadNextLevel();
         }
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel());
+    }
+
+    IEnumerator LoadLevel()
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transTime);
+
+        SceneManager.LoadScene(SceneIndex);
     }
 }
